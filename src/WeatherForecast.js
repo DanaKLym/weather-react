c4f82f9f361db059857924126a4ee528d1879e52
Dana Klymenko <bohdanamandziuk@gmail.com>
2022-12-20 10:14:16 +0200

    updated forecast for 5 days, fixed the bug with forecast info refreshing

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,14 @@
 import axios from "axios";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ForecastDay from "./ForecastDay";
 
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect (() => {
+        setLoaded(false);
+    }, [props.city]);
 
     function handleResponse(response) {
         setForecast(response.data.daily);
@@ -12,21 +16,24 @@ export default function WeatherForecast(props) {
     }
 
     if (loaded) {
-        console.log(forecast);
-        
         return (
             <div className="weatherForecast">
             <div className="row">
-                <div className="col">
-                    <ForecastDay data={forecast[0]}/>
-                </div>
+                {forecast.map(function(dailyForecast, index) {
+                    if (index < 5) {
+                        return (<div className="col" key={index}>
+                    <ForecastDay data={dailyForecast}/>
+                </div>);
+                    } else {
+                        return null;
+                    }
+                })}
             </div>
         </div>) } 
         else {
             let apiKey = "b91708t4bbb93aa86b666d50a5of7abe";
-            let forecastCity = props.city;
             let unit = "metric";
-            let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${forecastCity}&key=${apiKey}&units=${unit}`;
+            let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${props.city}&key=${apiKey}&units=${unit}`;
 
             axios.get(apiUrl).then(handleResponse);
 
